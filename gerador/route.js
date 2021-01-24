bfc12958901241bd9610e34ce21af5b6347cfe8f
vlsantana40 <vlsantana40@gmail.com>
2021-01-24 19:44:07 -0400

    ajustes e adicionado jwt

--- gerador/route.js
+++ gerador/route.js
@@ -23,24 +23,24 @@ module.exports = {
         stream.once('open', function (fd) {
 
             stream.write(`module.exports = function (app) {\n`);
+            stream.write(`let { verify } = require('../routes/middleware/authentication');`);
             stream.write(`\tvar controller = require('../controllers/api/${nome}.controller');\n`);
-            stream.write(`\tapp.get('/api/${nome}/', controller.findAll);\n`);
-            stream.write(`\tapp.get('/api/${nome}/:id', controller.findOne);\n`);
+            stream.write(`\tapp.get('/api/${nome}/', verify('roll ${nome}'), controller.findAll);\n`);
+            stream.write(`\tapp.get('/api/${nome}/:id', verify('one ${nome}'), controller.findOne);\n`);
 
             for (var i = 0; i < estrutura.length; i++) {
                 const item = estrutura[i];        
                 if(estrutura[i].props) {
                     if(estrutura[i].props[0] === "filtravel") {
                         const nomeFuncao = item.nome.charAt(0).toUpperCase() + item.nome.slice(1);                        
-                        stream.write(`\tapp.get('/api/${nome}/findby/${item.nome}/:${item.nome}', controller.findBy${nomeFuncao});\n`);
+                        stream.write(`\tapp.get('/api/${nome}/findby/${item.nome}/:${item.nome}', verify('find by ${item.nome}'), controller.findBy${nomeFuncao});\n`);
                     }
                 }                
             }
             
-
-            stream.write(`\tapp.post('/api/${nome}/', controller.create);\n`);
-            stream.write(`\tapp.put('/api/${nome}/:id', controller.update);\n`);
-            stream.write(`\tapp.delete('/api/${nome}/:id', controller.delete);\n`);
+            stream.write(`\tapp.post('/api/${nome}/', verify('add ${nome}'), controller.create);\n`);
+            stream.write(`\tapp.put('/api/${nome}/:id', verify('update ${nome}'), controller.update);\n`);
+            stream.write(`\tapp.delete('/api/${nome}/:id', verify('delete ${nome}'), controller.delete);\n`);
             stream.write(`\n\n`);
             stream.write(`/*custom*/`);
             stream.write(codeString);            
